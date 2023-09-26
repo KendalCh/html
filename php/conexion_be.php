b7bf7d928b98716d18fe36c46ad5075163cd9856
KendalCh <chaconarcek@gmail.com>
2023-09-26 14:34:05 -0500

    update

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,8 +1,8 @@
 <?php
 
-    /*$conexion = mysqli_connect("localhost", "root1", "", "login_register_db");
+    $conexion = mysqli_connect("localhost", "root1", "", "login_register_db");
 
-    if ($conexion) {
+    /*if ($conexion) {
         echo 'Conectado exitosamente a la Base de Datos';
     }else {
         echo 'No se ha podido conectar a la Base de Datos';
